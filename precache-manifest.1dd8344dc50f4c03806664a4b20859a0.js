93ea8634f7072e1dab5c5352069a817cac768b0d
Bharath Kumar <pbkbharath1707@gmail.com>
2020-08-06 08:27:15 +0530

    Updates

---
+++ precache-manifest.1dd8344dc50f4c03806664a4b20859a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3a38b5eaf69586b24104056613355d8",
+    "url": "/Youtube-Bharath/index.html"
+  },
+  {
+    "revision": "2d1f38e8495e6ae525d5",
+    "url": "/Youtube-Bharath/static/css/main.8a1be2ae.chunk.css"
+  },
+  {
+    "revision": "82fcf6b379ca84a04ed9",
+    "url": "/Youtube-Bharath/static/js/2.cf127c64.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Youtube-Bharath/static/js/2.cf127c64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d1f38e8495e6ae525d5",
+    "url": "/Youtube-Bharath/static/js/main.13f30822.chunk.js"
+  },
+  {
+    "revision": "0993b195e1f6807ef48f",
+    "url": "/Youtube-Bharath/static/js/runtime-main.b7d84992.js"
+  }
+]);
